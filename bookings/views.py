47025cd7c18e657ececd9758f25045acc188e454
Vvk_0105 <vivekbabu.sics@gmail.com>
2025-10-19 17:36:03 +0530

    testing

--- bookings/views.py
+++ bookings/views.py
@@ -1,10 +1,12 @@
+from django.utils.decorators import method_decorator 
 from django.shortcuts import render
 from rest_framework import generics
+from django.views.decorators.csrf import csrf_exempt
 from .models import Booking
 from .serializers import BookingSerializer
 
 # Create your views here.
-
+@method_decorator(csrf_exempt, name='dispatch') 
 class BookingListCreateView(generics.ListCreateAPIView):
     queryset = Booking.objects.all().order_by('-created_at')
     serializer_class = BookingSerializer
--- resort_backend/settings.py
+++ resort_backend/settings.py
@@ -62,7 +62,6 @@ CORS_ALLOW_HEADERS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
